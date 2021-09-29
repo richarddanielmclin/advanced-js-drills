614ff94666a9d901b659cd259591427f444724f2
unknown <richdanmac@gmail.com>
2021-09-29 00:08:53 -0500

    finished

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 name = 'Richard';
 var name;
 console.log(name);
+
+setName();
+function setName() {
+    var name = 'Covalence';
+    console.log(name);
+}
+
+console.log('some text');
+let avg = findAvg(8, 4);
+console.log('some text', avg);
+
+function findAvg(x, y) {
+    console.log('some text');
+    var answer = (x + y) / 2;
+    return answer;
+}
+
+let fruits = ['mango', 'plum', 'pear']
+
+function printFirstFruit() {
+    let faveFruit;
+    faveFruit = 'strawberry' 
+    console.log(fruits[0])
+    function printFaveFruit() {
+        let leastFav;
+        leastFav = 'papaya'
+        console.log(faveFruit)
+        console.log(leastFav)
+    }
+    printFaveFruit();
+}
+printFirstFruit();
+
+logHello();
+function logHello() {
+    console.log('Hello')
+}
+
+let showAlert = function () {
+    alert('If you see this, it worked!')
+}
+showAlert();
